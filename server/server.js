d766e93adb0bc17515270ff133d06912d0934a82
William Maguire <billymaguire3@gmail.com>
2020-09-24 11:28:50 -0400

    Implement file set-up necessary to run webpack. Test page render from App component inside index.jsx.

--- server/server.js
+++ server/server.js
@@ -3,11 +3,9 @@ const app = express()
 let port = 3004
 const path = require('path')
 
-app.use('/', express.static(path.join(__dirname, '')))
+app.use('/', express.static(path.join(__dirname, '/../client/dist')))
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+
 
 app.listen(port, () => {
   console.log(`Photos-Gallery App Listening on Port http://localhost:${port}`)
