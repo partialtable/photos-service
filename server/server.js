6f5223d4a44357bbdd99dd076f5047d596ce24d1
William Maguire <billymaguire3@gmail.com>
2020-09-24 17:50:11 -0400

    Install Body parser and require in server.js as well use url encoder and use json parser

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 const port = 3003;
 const path = require('path');
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, '/../client/dist')));
 
