ed8560f19d27f7e67f7e6895c60b3392f7845324
Theodore Nguyen <theodorenguyen5@gmail.com>
2020-10-24 14:25:39 -0700

    Changed get request within my k6 script

--- k6-script.js
+++ k6-script.js
@@ -3,10 +3,10 @@ import http from "k6/http";
 
 export const options = {
   stages: [
-    { duration: "1m", target: 200 },
-    { duration: "2m", target: 500 },
-    { duration: "3m", target: 1000 },
-    { duration: "1m", target: 200 },
+    { duration: "30s", target: 200 },
+    { duration: "30s", target: 500 },
+    { duration: "1m", target: 1000 },
+    { duration: "30s", target: 200 },
     { duration: "30s", target: 50 },
   ],
   ext: {
